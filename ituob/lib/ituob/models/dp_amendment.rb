f4ad8a4bb85a05d87046f7bc29a656fb3746077a
Ronald Tse <ronald.tse@ribose.com>
2025-05-19 15:35:55 +0800

    fix: dp_amendment

--- ituob/lib/ituob/models/dp_amendment.rb
+++ ituob/lib/ituob/models/dp_amendment.rb
@@ -40,14 +40,14 @@ module Ituob
           raise "Unexpected non-array item" unless c.is_a?(Array)
           next if c.flatten[0] && c.flatten[0].match(/^Country/)
 
-          first_elem = c[0] 
+          first_elem = c[0]
           if first_elem.is_a?(String)
-            next if first_elem.length < 3 
+            next if first_elem.length < 3
             next unless first_elem.match(/^P/)
             basestr = c.join('')
             segs = Ituob::Helpers.split_str(basestr)
 
-            @action = DPAction.new 
+            @action = DPAction.new
             @action.entries = []
             @action.position = segs[0..1].join(" ")
             if basestr.include?('LIR*')
@@ -59,26 +59,30 @@ module Ituob
             else
               @action.action_type = basestr[-3..-1]
             end
-            amendment.actions << @action 
+
+            amendment.actions << @action
 
           elsif first_elem.is_a?(Array) # table
 
             # they're all one row
-            @entry = DPEntry.new 
-            segs = c.flatten
+            @entry = DPEntry.new
+            segs = c.flatten.map { |x| Ituob::Helpers.strip_legacy(x) }
             @entry.country_or_area = MultilingualString.new(en: segs[0])
-            @entry.country_code = segs[1]
-            @entry.international_prefix = segs[2]
-            @entry.national_prefix = segs[3] 
-            @entry.national_sig_number = segs[4] 
-            @entry.utc_dst = segs[5]
-            @entry.note = segs[6]
+            @entry.country_code = segs[1] unless segs[1].nil? || segs[1].empty?
+            @entry.international_prefix = segs[2] unless segs[2].nil? || segs[2].empty?
+            @entry.national_prefix = segs[3] unless segs[3].nil? || segs[3].empty?
+            @entry.national_sig_number = segs[4] unless segs[4].nil? || segs[4].empty?
+            @entry.utc_dst = segs[5] unless segs[5].nil? || segs[5].empty?
+            @entry.note = MultilingualString.new(en: segs[6]) unless segs[6].nil? || segs[6].empty?
+
             @action.entries << @entry
           else
             next if first_elem.nil?
             raise "Unexpected non-string/array elem in c[0]"
           end
+
         end
+
         amendment
       end
 
