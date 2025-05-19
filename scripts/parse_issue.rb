07679a79aba02ef0f654270173bd42703ec2053a
Ronald Tse <ronald.tse@ribose.com>
2025-05-19 17:00:02 +0800

    feat: add script to show particular type of issue messages

--- scripts/parse_issue.rb
+++ scripts/parse_issue.rb
@@ -7,6 +7,33 @@ require 'ituob'
 # Get filter type from command line args
 filter_type = ARGV[0]
 
+# Display help information
+def show_help
+  puts "Usage: ruby parse_issue.rb [FILTER_TYPE | --help | -h]"
+  puts
+  puts "Description:"
+  puts "  Process ITU Operational Bulletin (OB) issue data and optionally filter by amendment or general message type."
+  puts
+  puts "Options:"
+  puts "  --help, -h    Show this help message"
+  puts "  FILTER_TYPE   Filter results by amendment type or general message type"
+  puts
+  puts "Supported Amendment Types:"
+  Ituob::Models::OldIssue::AMENDMENT_TYPE_TO_CLASS.keys.sort.each do |type|
+    puts "  #{type}"
+  end
+  puts
+  puts "Supported General Message Types:"
+  Ituob::Models::OldIssue::GENERAL_TYPE_TO_CLASS.keys.sort.each do |type|
+    puts "  #{type}"
+  end
+  puts
+  exit 0
+end
+
+# Check for help flags
+show_help if filter_type == "--help" || filter_type == "-h"
+
 # Path to amendment files
 ISSUE_DIRS = Dir.glob('../itu-ob-data/issues/*')
 # ISSUE_DIRS = [
@@ -27,37 +54,46 @@ ISSUE_DIRS.each do |issue_dir|
     next
   end
 
-  if filter_type
-    # Check if filter_type is an amendment type
-    if Ituob::Models::OldIssue::AMENDMENT_TYPE_TO_CLASS.keys.include?(filter_type)
-      # Get the class for this amendment type
-      amendment_class = Ituob::Models::OldIssue::AMENDMENT_TYPE_TO_CLASS[filter_type]
+  unless filter_type
+    # No filter specified, show all data
+    puts "%" * 80
+    puts issue_data.to_yaml
+    next
+  end
 
-      # Filter amendments by class
-      filtered_amendments = issue_data.amendments&.select { |a| a.is_a?(amendment_class) }
+  # Check if filter_type is an amendment type
+  if Ituob::Models::OldIssue::AMENDMENT_TYPE_TO_CLASS.keys.include?(filter_type)
+    # Get the class for this amendment type
+    amendment_class = Ituob::Models::OldIssue::AMENDMENT_TYPE_TO_CLASS[filter_type]
 
-      if filtered_amendments&.any?
-        puts "%" * 80
-        puts filtered_amendments.to_yaml
-      else
-        puts "[INFO] No amendments of type '#{filter_type}' found in #{issue_dir}"
-      end
-    # Check if filter_type is a general message type
-    elsif Ituob::Models::OldIssue::GENERAL_TYPE_TO_CLASS.keys.include?(filter_type)
-      if issue_data.general_messages&.any? { |g| g.type == filter_type }
-        puts "%" * 80
-        filtered_general = issue_data.general_messages.select { |g| g.type == filter_type }
-        puts filtered_general.to_yaml
-      else
-        puts "[INFO] No general messages of type '#{filter_type}' found in #{issue_dir}"
+    # Filter amendments by class
+    filtered_amendments = issue_data.amendments&.select { |a| a.is_a?(amendment_class) }
+
+    if filtered_amendments.any?
+      puts "%" * 80
+      filtered_amendments.each do |amendment|
+        puts amendment.to_yaml
       end
     else
-      puts "[WARNING] Unknown filter type: #{filter_type}. Showing all data."
-      puts "%" * 80
-      puts issue_data.to_yaml
+      puts "[INFO] No amendments of type '#{filter_type}' found in #{issue_dir}"
     end
+
+  # Check if filter_type is a general message type
+  elsif Ituob::Models::OldIssue::GENERAL_TYPE_TO_CLASS.keys.include?(filter_type)
+    general_class = Ituob::Models::OldIssue::GENERAL_TYPE_TO_CLASS[filter_type]
+    filtered_general = issue_data.general_messages.select { |g| g.is_a?(general_class) }
+
+    if filtered_general.any?
+      puts "%" * 80
+      filtered_general.each do |general|
+        puts general.to_yaml
+      end
+    else
+      puts "[INFO] No general messages of type '#{filter_type}' found in #{issue_dir}"
+    end
+
   else
-    # No filter specified, show all data
+    puts "[WARNING] Unknown filter type: #{filter_type}. Showing all data."
     puts "%" * 80
     puts issue_data.to_yaml
   end
